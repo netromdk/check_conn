df115645a0d2d308d53c9f8fc0d41391b9703eac
Morten Kristensen <msk@nullpointer.dk>
2015-11-11 19:13:49 +0100

    Changed TIMEOUT to INTERVAL.

--- check_conn.py
+++ check_conn.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 
 SITE = "https://google.com"
-TIMEOUT = 30 # seconds
+INTERVAL = 30 # seconds
 
 def notify(msg):
   subprocess.call(["terminal-notifier", "-message", msg])
@@ -13,7 +13,7 @@ if __name__ == "__main__":
   connected = True
 
   while True:
-    time.sleep(TIMEOUT)
+    time.sleep(INTERVAL)
     try:
       response = requests.get(SITE)
       if connected is False:
