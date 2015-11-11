34ce46179a3b63debf219cce8ce54f208eaa20d8
Morten Kristensen <msk@nullpointer.dk>
2015-11-11 18:38:26 +0100

    Cleaned up a little bit.

--- check_conn.py
+++ check_conn.py
@@ -3,19 +3,19 @@ import time
 import requests
 import subprocess
 
-connected = True
-
 def notify(msg):
-	subprocess.call(["terminal-notifier", "-message", msg])
+  subprocess.call(["terminal-notifier", "-message", msg])
 
-while(True):
-	time.sleep(60)
-	try:
-		response = requests.get("https://google.com")
-		if connected is False:
-			notify("Internet connection restored.")
-		connected = True
-	except:
-		if connected is not False:
-		  notify("Internet connection dropped.")
-		connected = False
+if __name__ == "__main__":
+  connected = True
+  while True:
+    time.sleep(60)
+    try:
+      response = requests.get("https://google.com")
+      if connected is False:
+        notify("Internet connection restored.")
+        connected = True
+    except:
+      if connected is not False:
+        notify("Internet connection dropped.")
+        connected = False
